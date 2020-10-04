a69d0814d1b0fb0cd37b6ea20a113be6e8a1ab05
“irobetson” <ianrobertson427@gmail.com>
2020-10-04 18:40:21 -0500

    Adding Basic Functioning Dummy Data, use DB:Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Celeb.create(name: "Cher", net: 305000000)
+Country.create(name: "Japan", ctc: 1869, sqft: 145882, pop: 126500000)
