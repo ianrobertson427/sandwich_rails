0c8632325af3c8ec50292711ba6cac4f6d9b0c6a
“irobetson” <ianrobertson427@gmail.com>
2020-10-04 21:06:15 -0500

    Adding floaters w/ khenriquez

--- app/controllers/api/calculator_controller.rb
+++ app/controllers/api/calculator_controller.rb
@@ -1,7 +1,8 @@
 class Api::CalculatorController < ApplicationController
   def average
-    #Displays in inches (63360 inches per mile)
-    @result = Celeb.find_by(id: 1).net / 5 * 12 / 63360
+    #Displays how many miles of sandwiches per celeb - HARDCODED
+    @result = Celeb.find_by(id: 1).net.to_f / 5 * 12 / 63360
+    @final_result = @result / Country.find_by(id: 1).ctc
     render "show.json.jb"
   end
 end
