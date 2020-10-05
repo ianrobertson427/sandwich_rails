1e78f75c34a72b49327cf1003436f165af4726c7
“irobetson” <ianrobertson427@gmail.com>
2020-10-04 20:58:42 -0500

    Adding Calculations w/ khenriquez

--- app/controllers/api/calculator_controller.rb
+++ app/controllers/api/calculator_controller.rb
@@ -1,6 +1,7 @@
 class Api::CalculatorController < ApplicationController
   def average
-    @result = Celeb.find_by(id: 1).net / Country.find_by(id: 1).ctc
+    #Displays in inches (63360 inches per mile)
+    @result = Celeb.find_by(id: 1).net / 5 * 12 / 63360
     render "show.json.jb"
   end
 end
